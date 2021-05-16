f497d11766a70f1da160b77f89e2970bcb54cf9e
Salomon-code <53943197+Salomon-code@users.noreply.github.com>
2021-05-15 22:16:55 -0300

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -304,6 +304,46 @@ void DFS(Vertice G[], int ordem) {
 	}
 }
 
+
+typedef struct reg {
+   int         conteudo; 
+   struct reg *prox;
+} celula;
+
+
+celula *fi;
+fi = malloc (sizeof (celula));
+fi->prox = fi;
+
+
+// Tira um elemento da fila fi e devolve
+// o conteudo do elemento removido.
+// Supõe que a fila não está vazia.
+
+int tiradafila (celula *fi) {
+   celula *p;
+   p = fi->prox;  // o primeiro da fila
+   int x = p->conteudo;
+   fi->prox = p->prox;
+   free (p);
+   return x;  
+}
+
+
+// Coloca um novo elemento com conteudo y
+// na fila fi. Devolve o endereço da
+// cabeça da fila resultante.
+
+celula *colocanafila (int y, celula *fi) { 
+   celula *nova;
+   nova = malloc (sizeof (celula));
+   nova->prox = fi->prox;
+   fi->prox = nova;
+   fi->conteudo = y;
+   return nova;
+}
+
+
 void BFS(Vertice G[], int ordem, int s){
 
   
